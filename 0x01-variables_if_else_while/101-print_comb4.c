14d896a3b5a683b746617969e1f30192935cb372
BlesynChinny <blessingchineyenwa43@gmail.com>
2023-02-20 16:47:39 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - The main function
@@ -14,23 +12,23 @@ int main(void)
 	for (i = 0; i < 7; i++)
 	for (i = 0; i < 8; i++)
 	{
-	for (j = i + 1; j < 8; j++)
-	for (j = i + 1; j < 9; j++)
-	{
-	for (k = j + 1; k < 9; k++)
-	for (k = j + 1; k < 10; k++)
-	{
-	putchar(i + '0');
-	putchar(j + '0');
-	putchar(k + '0');
-	if (i < 7)
-	{
-	putchar(',');	
-	putchar(' ');
-	}
-	}
-	}
-	}
+		for (j = i + 1; j < 8; j++)
+		for (j = i + 1; j < 9; j++)
+		{
+		for (k = j + 1; k < 9; k++)
+		for (k = j + 1; k < 10; k++)
+		{
+		putchar(i + '0');
+		putchar(j + '0');
+		putchar(k + '0');
+		if (i < 7)
+		{		
+		putchar(',');	
+		putchar(' ');
+		}
+		}
+		}		
+		}	
 	putchar('\n');
 	return (0);
 }
