c75424d31637106b2c66f2105c7190ce4939ca49
BlesynChinny <blessingchineyenwa43@gmail.com>
2023-02-22 19:24:23 -0800

    Task 16

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	for (; n <= 98; n++)
+	for (n <= 98; n++)
 	{
 	if (n == 98)
 	{
@@ -30,7 +30,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	for (; n >= 98; n--)
+	for (n >= 98; n--)
 	{
 	if (n == 98)
 	{
@@ -42,7 +42,7 @@ void print_to_98(int n)
 	{
 	printf("%d, ", n);
 	}
-}	
+	}	
 	}
 }
 
