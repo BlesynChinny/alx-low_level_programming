ec9bbd621c22b5653ab233bff84a6d6151c3a8cc
BlesynChinny <blessingchineyenwa43@gmail.com>
2023-03-03 16:49:31 +0100

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#inlcude "mian.h"
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if ([i] >='a' && n [i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
